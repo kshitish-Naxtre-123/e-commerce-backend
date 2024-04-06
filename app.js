a434ed0f4f70f449b9b7f105e273d0817bf1dc1e
KSHITISH NAYAK <157575372+kshitish-Naxtre-123@users.noreply.github.com>
2024-04-06 11:20:04 +0530

    updated url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(
-    origin:*,
+    origin:"https://e-cart-india.netlify.app",
     credentials: true,
 ));
 
