45c0f29704858946283bda943a44a4dfbd2784d7
kshitish-Naxtre-123 <kshitish.naxtre@gmail.com>
2024-04-06 15:03:59 +0530

    set credentials

--- utils/createToken.js
+++ utils/createToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   //set jwt as a Http-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
