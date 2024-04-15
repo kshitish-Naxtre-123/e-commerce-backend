915cc6c1093208ce0dd151ef85294fce167fdf8c
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-04-15 12:35:59 +0530

    chang tax rate

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,20 +3,13 @@ import Product from "../models/productModel.js";
 
 //utility function
 function calcPrices(orderItems){
-    // const itemsPrice=Number(orderItems.reduce(
-    //     (acc,item)=>acc+item.price*item.qty,0
-    // ))
-    // const shippingPrice=itemsPrice>100?0:10
-    // const taxRate=0.15
-    // const taxPrice=Number((itemsPrice*taxRate).toFixed(2))
-    // const totalPrice=(
-    //     itemsPrice+shippingPrice+Number(parseFloat(taxPrice).toFixed(2))
+
     // )
     const itemsPrice=orderItems.reduce(
         (acc,item)=>acc+item.price*item.qty,0
     )
     const shippingPrice=itemsPrice>100?0:10
-    const taxRate=0.15
+    const taxRate=0.0
     const taxPrice=(itemsPrice*taxRate).toFixed(2)
     const totalPrice=(
         itemsPrice+shippingPrice+parseFloat(taxPrice)).toFixed(2)
