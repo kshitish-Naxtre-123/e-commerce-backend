d7f31a1925813cc60e406d435f79e517675e2b8f
KSHITISH NAYAK <157575372+kshitish-Naxtre-123@users.noreply.github.com>
2024-04-17 18:36:16 +0530

    add netlify origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "https://shop-with-me.netlify.app/",
+    // origin: "https://e-cart-india.netlify.app/",
     credentials: true,
   })
 );
